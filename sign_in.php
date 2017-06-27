b815b75e7ee3e696ff0babfbad3f7845f9137a37
Jony <liu.z.jonathan@gmail.com>
2017-06-27 12:31:06 -0700

    erik is weird

--- sign_in.php
+++ sign_in.php
@@ -67,6 +67,8 @@
 			//initialize vars
 			$nameErr = $emailErr = $instagramErr = $countErr = $schoolErr = $stateErr = $methodErr = "";
 			$name = $email = $instagram = $count = $school = $state = $method = "";
+			$state = "sign in";
+			
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				$valid = true;
 
@@ -132,12 +134,13 @@
 				} else {
 					$location = test_input($_POST["location"]);
 				}
-        if (empty($_POST["method"])) {
-          $methodErr = "Your payment method is required.";
-          $valid = false;
-        } else {
-          $method = test_input($_POST["method"]);
-        }
+					
+				if (empty($_POST["method"])) {
+				  $methodErr = "Your payment method is required.";
+				  $valid = false;
+				} else {
+				  $method = test_input($_POST["method"]);
+				}
 
 				if ($valid) {
 					//proceed
@@ -160,7 +163,7 @@
       <h3 class="section-title">Sign up!</h3>
 
   		<p><span class="error">* required field.</span></p>
-          <form method="post" action="form_process.php" class="contact-form">
+          <form method="post" class="contact-form">
 			<div class="form-group">
         <select name="method" id="method">
             <option value="">How would you like to get paid?</option>
@@ -250,21 +253,38 @@
                       <input type="text" name="count" id="count" value="" placeholder="CURRENT Instagram Follower Count" />
   					<span class="error">* <?php echo $countErr;?></span>
                   </div>
-              </form>
-
-
   				<div class="form-group">
                       <ul class="actions">
                           <td><input type="submit" name ="submit" value="Submit"/></td>
                       </ul>
                   </div>
+				  
+				</form>  
             </div>
           </section>
         </body>
       </html>
 		<?php
 			} else if ($state == "store data") {
-
+				$method = $_POST["method"];
+				$paypal = $_POST["paypal"];
+				$name = $_POST["name"];
+				$email = $_POST["email"];
+				$instagram = $_POST["instagram"];
+				$count = $_POST["count"];
+				$school = $_POST["school"];
+				$state = $_POST["location"];
+				$db = new SQLite3('userinfo.db');
+				$db->exec(" CREATE TABLE IF NOT EXISTS users (method TEXT NOT NULL,paypal TEXT,
+				name TEXT NOT NULL,email TEXT NOT NULL, instagram TEXT NOT NULL, count INTEGER NOT NULL,
+				 school TEXT NOT NULL, state TEXT NOT NULL)");
+				$db->exec("INSERT INTO users"."(method, paypal, name, email, instagram, count, school, state)"." VALUES
+				('$method', '$paypal', '$name', '$email', '$instagram', $count, '$school', '$state');");
+				$db->close();
+			
+		?>
+		<h1>Thank you for signing up!</h1>
+		<?php
 			}
 		?>
       </body>

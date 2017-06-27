555b7c9a58e8a640f496e838a6b37c3378752930
Jony <liu.z.jonathan@gmail.com>
2017-06-27 15:10:58 -0700

    conflict fuck

--- sign_in.php
+++ sign_in.php
@@ -66,12 +66,12 @@
 	</header>
 		<?
 			//initialize vars
-			$nameErr = $emailErr = $instagramErr = $countErr = $schoolErr = $stateErr = $methodErr = "";
-			$name = $email = $instagram = $count = $school = $state = $method = "";
+			$nameErr = $emailErr = $instagramErr = $countErr = $schoolErr = $stateErr = $methodErr = $paypalErr = "";
+			$name = $email = $instagram = $count = $school = $state = $method = $paypal = "";
 			$state = "sign in";
 			
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
-				$valid = true;
+				$valid = true;	
 
 				if (empty($_POST["name"])) {
 					$nameErr = "Name is required";
@@ -116,7 +116,7 @@
 					$valid = false;
 				} else {
 					$count = test_input($_POST["count"]);
-					if (filter_var($count, FILTER_VALIDATE_INT)) {
+					if (!filter_var($count, FILTER_VALIDATE_INT)) {
 						$countErr = "Please enter a number.";
 						$valid = false;
 					}
@@ -130,17 +130,30 @@
 				}
 
 				if (empty($_POST["location"])) {
-					$locationErr = "Your location is required.";
+					$locationErr = "Your state is required.";
 					$valid = false;
 				} else {
 					$location = test_input($_POST["location"]);
 				}
 					
 				if (empty($_POST["method"])) {
-				  $methodErr = "Your payment method is required.";
-				  $valid = false;
+					$methodErr = "Your payment method is required.";
+					$valid = false;
 				} else {
-				  $method = test_input($_POST["method"]);
+					$method = test_input($_POST["method"]);
+					if ($method == "Paypal") {
+						if (empty($_POST["paypal"])) {
+							$methodErr = "Your Paypal email is required.";
+							$valid = false;
+						} else {
+							$paypal = test_input($_POST["paypal"]);
+							// check if e-mail address is well-formed
+							if (!filter_var($paypal, FILTER_VALIDATE_EMAIL)) {
+								$paypalErr = "Invalid email format";
+								$valid = false;
+							}
+						}
+					}
 				}
 
 				if ($valid) {
@@ -167,92 +180,95 @@
           <form method="post" class="contact-form">
 			<div class="form-group">
         <select name="method" id="method">
-            <option value="">How would you like to get paid?</option>
-			<option value="Paypal" id="paypal-option">Paypal</option>
-			<option value="Amazon" id="amazon-option">Amazon Gift Card</option>
+            <option <?php if ($_POST['method'] == "") {?> selected="true" <?php }; ?> value="">How would you like to get paid?</option>
+			<option <?php if ($_POST['method'] == "Paypal") {?> selected="true" <?php }; ?> value="Paypal" id="paypal-option">Paypal</option>
+			<option <?php if ($_POST['method'] == "Amazon") {?> selected="true" <?php }; ?> value="Amazon" id="amazon-option">Amazon Gift Card</option>
 		</select>
 					
            <span class="error">* <?php echo $methodErr;?></span>
         </div>
-  				<div class="form-group">
-                <input type="text" name="paypal" id="paypal" value="<?php echo $paypal;?>" placeholder="Enter your Paypal email" style="display:none;"/>
-            </div>
-  				<div class="form-group">
-                <input type="text" name="name" id="name" value="<?php echo $name;?>" placeholder="Name"/>
-  					<span class="error">* <?php echo $nameErr;?></span>
-          </div>
-  				<div class="form-group">
-                      <input type="email" name="email" id="email" value="<?php echo $email;?>" placeholder="Email" />
-  					<span class="error">* <?php echo $emailErr;?></span>
-          </div>
-  				<div class="form-group">
-                      <input type="text" name="instagram" id="instagram" value="<?php echo $instagram;?>" placeholder="@InstagramHandle" />
-  					<span class="error">* <?php echo $instagramErr;?></span>
-          </div>
-  				<div class="form-group">
-  					<input type="text" name="school" id="school" value="<?php echo $school;?>" placeholder="School" />
-  					<span class="error">* <?php echo $schoolErr;?></span>
-  				</div>
-  				<div class="form-group">
-            <select name="state" id="state">
-                <option value="">What state do you live in?</option>
-                <option value="AL">Alabama</option>
-				<option value="AK">Alaska</option>
-				<option value="AZ">Arizona</option>
-				<option value="AR">Arkansas</option>
-				<option value="CA">California</option>
-				<option value="CO">Colorado</option>
-				<option value="CT">Connecticut</option>
-				<option value="DE">Delaware</option>
-				<option value="DC">District Of Columbia</option>
-				<option value="FL">Florida</option>
-				<option value="GA">Georgia</option>
-				<option value="HI">Hawaii</option>
-				<option value="ID">Idaho</option>
-				<option value="IL">Illinois</option>
-				<option value="IN">Indiana</option>
-				<option value="IA">Iowa</option>
-				<option value="KS">Kansas</option>
-				<option value="KY">Kentucky</option>
-				<option value="LA">Louisiana</option>
-				<option value="ME">Maine</option>
-				<option value="MD">Maryland</option>
-				<option value="MA">Massachusetts</option>
-				<option value="MI">Michigan</option>
-				<option value="MN">Minnesota</option>
-				<option value="MS">Mississippi</option>
-				<option value="MO">Missouri</option>
-				<option value="MT">Montana</option>
-				<option value="NE">Nebraska</option>
-				<option value="NV">Nevada</option>
-				<option value="NH">New Hampshire</option>
-				<option value="NJ">New Jersey</option>
-				<option value="NM">New Mexico</option>
-				<option value="NY">New York</option>
-				<option value="NC">North Carolina</option>
-				<option value="ND">North Dakota</option>
-				<option value="OH">Ohio</option>
-				<option value="OK">Oklahoma</option>
-				<option value="OR">Oregon</option>
-				<option value="PA">Pennsylvania</option>
-				<option value="RI">Rhode Island</option>
-				<option value="SC">South Carolina</option>
-				<option value="SD">South Dakota</option>
-				<option value="TN">Tennessee</option>
-				<option value="TX">Texas</option>
-				<option value="UT">Utah</option>
-				<option value="VT">Vermont</option>
-				<option value="VA">Virginia</option>
-				<option value="WA">Washington</option>
-				<option value="WV">West Virginia</option>
-				<option value="WI">Wisconsin</option>
-				<option value="WY">Wyoming</option>
+		<div class="form-group">
+			<div id="paypal-div" <?php if ($_POST['method'] == "Paypal") {?> style="display:block;" <?php }; ?> style="display:none;">
+				<input type="text" name="paypal" id="paypal" value="<?php echo $paypal;?>" placeholder="Enter your Paypal email" />
+				<span class="error">* <?php echo $paypalErr;?></span>
+			</div>
+		</div>
+		<div class="form-group">
+			<input type="text" name="name" id="name" value="<?php echo $name;?>" placeholder="Name"/>
+			<span class="error">* <?php echo $nameErr;?></span>
+		</div>
+		<div class="form-group">
+			<input type="text" name="email" id="email" value="<?php echo $email;?>" placeholder="Email" />
+			<span class="error">* <?php echo $emailErr;?></span>
+		</div>
+		<div class="form-group">
+			<input type="text" name="instagram" id="instagram" value="<?php echo $instagram;?>" placeholder="@InstagramHandle" />
+			<span class="error">* <?php echo $instagramErr;?></span>
+        </div>
+		<div class="form-group">
+			<input type="text" name="school" id="school" value="<?php echo $school;?>" placeholder="School" />
+			<span class="error">* <?php echo $schoolErr;?></span>
+		</div>
+  		<div class="form-group">
+            <select name="location" id="location">
+                <option <?php if ($_POST['location'] == "") {?> selected="true" <?php }; ?> value="">What state do you live in?</option>
+                <option <?php if ($_POST['location'] == "AL") {?> selected="true" <?php }; ?> value="AL">Alabama</option>
+				<option <?php if ($_POST['location'] == "AK") {?> selected="true" <?php }; ?> value="AK">Alaska</option>
+				<option <?php if ($_POST['location'] == "AZ") {?> selected="true" <?php }; ?> value="AZ">Arizona</option>
+				<option <?php if ($_POST['location'] == "AR") {?> selected="true" <?php }; ?> value="AR">Arkansas</option>
+				<option <?php if ($_POST['location'] == "CA") {?> selected="true" <?php }; ?> value="CA">California</option>
+				<option <?php if ($_POST['location'] == "CO") {?> selected="true" <?php }; ?> value="CO">Colorado</option>
+				<option <?php if ($_POST['location'] == "CT") {?> selected="true" <?php }; ?> value="CT">Connecticut</option>
+				<option <?php if ($_POST['location'] == "DE") {?> selected="true" <?php }; ?> value="DE">Delaware</option>
+				<option <?php if ($_POST['location'] == "DC") {?> selected="true" <?php }; ?> value="DC">District Of Columbia</option>
+				<option <?php if ($_POST['location'] == "FL") {?> selected="true" <?php }; ?> value="FL">Florida</option>
+				<option <?php if ($_POST['location'] == "GA") {?> selected="true" <?php }; ?> value="GA">Georgia</option>
+				<option <?php if ($_POST['location'] == "HI") {?> selected="true" <?php }; ?> value="HI">Hawaii</option>
+				<option <?php if ($_POST['location'] == "ID") {?> selected="true" <?php }; ?> value="ID">Idaho</option>
+				<option <?php if ($_POST['location'] == "IL") {?> selected="true" <?php }; ?> value="IL">Illinois</option>
+				<option <?php if ($_POST['location'] == "IN") {?> selected="true" <?php }; ?> value="IN">Indiana</option>
+				<option <?php if ($_POST['location'] == "IA") {?> selected="true" <?php }; ?> value="IA">Iowa</option>
+				<option <?php if ($_POST['location'] == "KS") {?> selected="true" <?php }; ?> value="KS">Kansas</option>
+				<option <?php if ($_POST['location'] == "KY") {?> selected="true" <?php }; ?> value="KY">Kentucky</option>
+				<option <?php if ($_POST['location'] == "LA") {?> selected="true" <?php }; ?> value="LA">Louisiana</option>
+				<option <?php if ($_POST['location'] == "ME") {?> selected="true" <?php }; ?> value="ME">Maine</option>
+				<option <?php if ($_POST['location'] == "MD") {?> selected="true" <?php }; ?> value="MD">Maryland</option>
+				<option <?php if ($_POST['location'] == "MA") {?> selected="true" <?php }; ?> value="MA">Massachusetts</option>
+				<option <?php if ($_POST['location'] == "MI") {?> selected="true" <?php }; ?> value="MI">Michigan</option>
+				<option <?php if ($_POST['location'] == "MN") {?> selected="true" <?php }; ?> value="MN">Minnesota</option>
+				<option <?php if ($_POST['location'] == "MS") {?> selected="true" <?php }; ?> value="MS">Mississippi</option>
+				<option <?php if ($_POST['location'] == "MO") {?> selected="true" <?php }; ?> value="MO">Missouri</option>
+				<option <?php if ($_POST['location'] == "MT") {?> selected="true" <?php }; ?> value="MT">Montana</option>
+				<option <?php if ($_POST['location'] == "NE") {?> selected="true" <?php }; ?> value="NE">Nebraska</option>
+				<option <?php if ($_POST['location'] == "NV") {?> selected="true" <?php }; ?> value="NV">Nevada</option>
+				<option <?php if ($_POST['location'] == "NH") {?> selected="true" <?php }; ?> value="NH">New Hampshire</option>
+				<option <?php if ($_POST['location'] == "NJ") {?> selected="true" <?php }; ?> value="NJ">New Jersey</option>
+				<option <?php if ($_POST['location'] == "NM") {?> selected="true" <?php }; ?> value="NM">New Mexico</option>
+				<option <?php if ($_POST['location'] == "NY") {?> selected="true" <?php }; ?> value="NY">New York</option>
+				<option <?php if ($_POST['location'] == "NC") {?> selected="true" <?php }; ?> value="NC">North Carolina</option>
+				<option <?php if ($_POST['location'] == "ND") {?> selected="true" <?php }; ?> value="ND">North Dakota</option>
+				<option <?php if ($_POST['location'] == "OH") {?> selected="true" <?php }; ?> value="OH">Ohio</option>
+				<option <?php if ($_POST['location'] == "OK") {?> selected="true" <?php }; ?> value="OK">Oklahoma</option>
+				<option <?php if ($_POST['location'] == "OR") {?> selected="true" <?php }; ?> value="OR">Oregon</option>
+				<option <?php if ($_POST['location'] == "PA") {?> selected="true" <?php }; ?> value="PA">Pennsylvania</option>
+				<option <?php if ($_POST['location'] == "RI") {?> selected="true" <?php }; ?> value="RI">Rhode Island</option>
+				<option <?php if ($_POST['location'] == "SC") {?> selected="true" <?php }; ?> value="SC">South Carolina</option>
+				<option <?php if ($_POST['location'] == "SD") {?> selected="true" <?php }; ?> value="SD">South Dakota</option>
+				<option <?php if ($_POST['location'] == "TN") {?> selected="true" <?php }; ?> value="TN">Tennessee</option>
+				<option <?php if ($_POST['location'] == "TX") {?> selected="true" <?php }; ?> value="TX">Texas</option>
+				<option <?php if ($_POST['location'] == "UT") {?> selected="true" <?php }; ?> value="UT">Utah</option>
+				<option <?php if ($_POST['location'] == "VT") {?> selected="true" <?php }; ?> value="VT">Vermont</option>
+				<option <?php if ($_POST['location'] == "VA") {?> selected="true" <?php }; ?> value="VA">Virginia</option>
+				<option <?php if ($_POST['location'] == "WA") {?> selected="true" <?php }; ?> value="WA">Washington</option>
+				<option <?php if ($_POST['location'] == "WV") {?> selected="true" <?php }; ?> value="WV">West Virginia</option>
+				<option <?php if ($_POST['location'] == "WI") {?> selected="true" <?php }; ?> value="WI">Wisconsin</option>
+				<option <?php if ($_POST['location'] == "WY") {?> selected="true" <?php }; ?> value="WY">Wyoming</option>
                           </select>
-                  <span class="error">* <?php echo $stateErr;?></span>
+                  <span class="error">* <?php echo $locationErr;?></span>
                 </div>
 
   				<div class="form-group">
-                      <input type="text" name="count" id="count" value="" placeholder="CURRENT Instagram Follower Count" />
+                      <input type="text" name="count" id="count" value="<?php echo $count;?>" placeholder="CURRENT Instagram Follower Count" />
   					<span class="error">* <?php echo $countErr;?></span>
                   </div>
   				<div class="form-group">
@@ -290,20 +306,28 @@
 		<script>
 		$(document).ready(function(){
 			$("#method").change(function(){
-				var value = $(this).find("option:selected").attr("value");
+				changeMethod();
+			});
+			
+			$( "#submit" ).click(function() {
+				changeMethod(	);
+			});
+			
+			function changeMethod() {
+				var value = $('#method').find("option:selected").attr("value");
 				
 				switch (value) {
 					case "Amazon":
-						$("#paypal").hide();
+						$("#paypal-div").hide();
 						break;
 					case "Paypal":
-						$("#paypal").show();
+						$("#paypal-div").show();
 						break;
 					default:
-						$("#paypal").hide();
+						$("#paypal-div").hide();
 						break;
 				}
-			});
+			}
 		});
 		</script>
       </body>

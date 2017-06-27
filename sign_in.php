b467630d892cb200bd2278bde342dc922810bf7f
Jony <liu.z.jonathan@gmail.com>
2017-06-26 23:25:15 -0700

    need some way to run validation WITH submission

--- sign_in.php
+++ sign_in.php
@@ -31,56 +31,73 @@
 			$name = $email = $instagram = $count = $school = $state = "";
 			
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				$valid = true;
+				
 				if (empty($_POST["name"])) {
 					$nameErr = "Name is required";
+					$valid = false;
 				} else {
 					$name = test_input($_POST["name"]);
 					// check if name only contains letters and whitespace
 					if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
 						$nameErr = "Only letters and white space allowed"; 
+						$valid = false;
 					}
 				}
 				
 				if (empty($_POST["email"])) {
 					$emailErr = "Email is required";
+					$valid = false;
 				} else {
 					$email = test_input($_POST["email"]);
 					// check if e-mail address is well-formed
 					if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 						$emailErr = "Invalid email format"; 
-					}
+						$valid = false;
+					} 
 				}
 				
 				if (empty($_POST["instagram"])) {
 					$instagramErr = "Your instagram handle is required.";
+					$valid = false;
 				} else {
 					$instagram = test_input($_POST["instagram"]);
 					if (preg_match('/\s/',$instagram)) {
 						$instagramErr = "No white space allowed."; 
+						$valid = false;
 					} else if (!substr($instagram, 0, 1) == "@") {
 						$instagramErr = "Your Instagram Handle must start with @.";
+						$valid = false;
 					}
 				}
 				
 				if (empty($_POST["count"])) {
 					$countErr = "Your follower count is required.";
+					$valid = false;
 				} else {
 					$count = test_input($_POST["count"]);
 					if (filter_var($count, FILTER_VALIDATE_INT)) {
 						$countErr = "Please enter a number."; 
+						$valid = false;
 					}
 				}
 				
 				if (empty($_POST["school"])) {
 					$schoolErr = "Your school is required.";
+					$valid = false;
 				} else {
 					$school = test_input($_POST["school"]);
 				}
 				
 				if (empty($_POST["state"])) {
 					$stateErr = "Your state is required.";
+					$valid = false;
 				} else {
 					$state = test_input($_POST["state"]);
+				}
+				
+				if ($valid) {
+					//proceed
 				}
 			}
 			
@@ -95,7 +112,7 @@
         <h3 class="align-center">Form</h3>
 		
 		<p><span class="error">* required field.</span></p>
-        <form method="post" action="form_process.php">
+        <form method="post">
             <div class="row uniform">
 				<div class="12u$(xsmall)">
 				<div class="select-wrapper">
@@ -193,7 +210,7 @@
 			
                 <div class="12u$">
                     <ul class="actions">
-                        <td><input type="submit" name ="b1" value="Submit" onClick="store.php"/></td>
+                        <td><input type="submit" name ="submit" value="Submit"/></td>
                     </ul>
                 </div>
       </body>
